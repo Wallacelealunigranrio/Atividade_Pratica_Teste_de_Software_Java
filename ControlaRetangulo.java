fba4a6b7c3b4b1df5c54f9f74422145385de4104
Wallacelealunigranrio <62752944+Wallacelealunigranrio@users.noreply.github.com>
2022-04-25 19:06:34 -0300

    Add files via upload

---
+++ ControlaRetangulo.java
@@ -0,0 +1,23 @@
+package controle;
+import dominio.Retangulo;
+import java.util.Scanner;
+
+public class ControlaRetangulo {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        
+        System.out.print("Digite o primeiro a base: ");
+        int base = leia.nextInt();
+        System.out.print("Digite o segundo a altura : ");
+        int altura = leia.nextInt();
+        leia.close();
+        
+        Retangulo res = new Retangulo(base, altura);
+        
+        System.out.println("RESULTADOS");
+        System.out.println("Area :" + res.area());
+        System.out.println("Perímetro :" + res.perimetro());
+       
+    }
+}
